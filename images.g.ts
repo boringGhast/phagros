0e7ff66bbbe51292e0f21786de62e74c35948ad7
boringGhast <103174425+boringGhast@users.noreply.github.com>
2022-06-17 07:18:27 +0800

    MakeCode 项目的初始文件

---
+++ images.g.ts
@@ -0,0 +1,42 @@
+// 自动生成的代码。请勿编辑。
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Tap":return img`
+1 8 8 8 8 8 8 8 8 8 8 8 8 8 8 1 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+1 8 8 8 8 8 8 8 8 8 8 8 8 8 8 1 
+`;
+            case "image4":
+            case "Hold-body":return img`
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+`;
+            case "image3":
+            case "Hold-foot":return img`
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 1 1 1 1 1 1 1 1 1 1 1 1 1 1 9 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// 自动生成的代码。请勿编辑。
